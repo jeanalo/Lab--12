327a4464d54a50679616f0419d00c88a7d4955e3
Jean alomia <Jeanalo>
2024-05-08 20:51:32 -0500

    UpdateLab13SearchBarAndTrash

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-   fetch('https://valorant-api.com/v1/agents')
-      .then(response => response.json())
-      .then(data => {
-        const cardContainer = document.querySelector('.card-container');
-     data.data.forEach(personaje => {
-          const card = document.createElement('div');
-          card.classList.add('card');
-          card.innerHTML = `
-            <img src="${personaje.displayIcon}" alt="Foto de ${personaje.displayName}">
-            <div>
-           <h2>${personaje.displayName}</h2>
+  fetch('https://valorant-api.com/v1/agents')
+    .then(response => response.json())
+    .then(data => {
+      const cardContainer = document.querySelector('.card-container');
+      const searchInput = document.getElementById('searchInput');
+
+      data.data.forEach(personaje => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <img src="${personaje.displayIcon}" alt="Foto de ${personaje.displayName}">
+          <div>
+            <h2>${personaje.displayName}</h2>
             <p>${personaje.description}</p>
             <button onclick="verInformacionDetallada('${personaje.displayName}')">Ver información detallada</button>
-            <i class="fas fa-trash"></i>
-           </div>
-          `;
-    cardContainer.appendChild(card);
+            <i class="fa-solid fa-trash" onclick="eliminarPersonaje(this)"></i>
+          </div>
+        `;
+        cardContainer.appendChild(card);
       });
-      })
-      .catch(error => {
-        console.log('Hubo un problema con la petición Fetch:' + error.message);
+
+      searchInput.addEventListener('input', function() {
+        const searchText = searchInput.value.toLowerCase();
+        const cards = cardContainer.getElementsByClassName('card');
+        for (let i = 0; i < cards.length; i++) {
+          const personajeName = cards[i].querySelector('h2').textContent.toLowerCase();
+          if (personajeName.includes(searchText)) {
+            cards[i].style.display = 'flex';
+          } else {
+            cards[i].style.display = 'none';
+          }
+        }
       });
-  });
-  
-  function verInformacionDetallada(displayName) {
-    window.location.href = `informacion-detallada.html?personaje=${encodeURIComponent(displayName)}`;
-  }
-  
+    })
+    .catch(error => {
+      console.log('Hubo un problema con la petición Fetch:' + error.message);
+    });
+});
+
+function verInformacionDetallada(displayName) {
+  window.location.href = `informacion-detallada.html?personaje=${encodeURIComponent(displayName)}`;
+}
+
+function eliminarPersonaje(button) {
+  const card = button.closest('.card');
+  card.remove();
+}
